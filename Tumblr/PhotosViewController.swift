13d208ebef554bdf1198f42f99b62f0bf8fe7bb4
Leslie <Leslie@Mayas-MacBook-Pro-2.local>
2018-10-01 19:08:36 -0400

    submission

---
+++ Tumblr/PhotosViewController.swift
@@ -0,0 +1,65 @@
+//
+//  PhotosViewController.swift
+//  Tumblr
+//
+//  Created by Leslie  on 10/1/18.
+//  Copyright © 2018 Leslie . All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var posts: [[String: Any]] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.rowHeight = 200
+        
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            //This will run when network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+                //print (self.posts)
+                self.tableView.reloadData()
+            }
+        }
+        task.resume()
+    }
+    
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.posts.count
+    }
+
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        
+        let post = self.posts[indexPath.row]
+        if let photos = post["photos"] as? [[String: Any]] {
+    
+            let photo = photos[0]
+            let originalSize = photo["original_size"] as! [String: Any]
+            let urlString = originalSize["url"] as! String
+            let url = URL(string: urlString)
+    
+        cell.photoImageView.af_setImage(withURL: url!)
+            }
+    
+        return cell
+        }
+}
+
